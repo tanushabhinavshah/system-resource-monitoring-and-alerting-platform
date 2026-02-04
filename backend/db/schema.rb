12da23ff4eeeb5455f45f90c75fde095efd50a35
tanushabhinavshah <tanush.shah@joshsoftware.com>
2026-02-04 15:56:26 +0530

    Changed column event_type to is_resolved in alerts table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_04_055547) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_04_094140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "alerts", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "event_type"
+    t.boolean "is_resolved", default: false, null: false
     t.text "reason"
     t.string "resource_type"
     t.string "severity"
